729693b440ee053c2954aac238126e7100e45531
unknown <nth.alexx@gmail.com>
2017-09-30 21:36:48 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,42 @@ var cssnano = require('cssnano');
 var autoprefixer = require('autoprefixer');
 var mqpacker = require("css-mqpacker");
 
+var watchify = require('watchify');
+var browserify = require('browserify');
+
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 var plumber = require('gulp-plumber');
 var notify = require("gulp-notify");
+var source = require('vinyl-source-stream');
 
+/* browserify */
+gulp.task('js:build', function() {
+  return browserify({
+    entries: ['./src/js/main.js'],
+    debug: true
+  })
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('./js'));
+});
 
-gulp.task('dev', ['sass'], function() {
-  browserSync.init({
-    server: './',
-    open: false,
-    notify: false
-  });
+/* watchify */
+gulp.task('js:watch', function () {
+  var b = watchify(browserify({
+    entries: ['./src/js/main.js'],
+    debug: true
+  }));
+  b.on('update', bundle);
 
-  gulp.watch('./src/sass/**/*.{sass,scss}', ['sass']);
-  gulp.watch('./*.html').on('change', browserSync.reload);
-  gulp.watch('src/**/*.html', ['copy']);
+  function bundle() {
+    return b.bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream());
+  }
+
+  return bundle();
 });
 
 gulp.task('copy', function () {
@@ -50,6 +70,17 @@ gulp.task('sass', function() {
     .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
+gulp.task('dev', ['build', 'js:watch'], function() {
+  browserSync.init({
+    server: './',
+    open: false,
+    notify: false
+  });
 
-gulp.task('build', ['sass', 'copy']);
-gulp.task('default', ['build', 'dev']);
+  gulp.watch('./src/sass/**/*.{sass,scss}', ['sass']);
+  gulp.watch('src/**/*.html', ['copy']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+gulp.task('build', ['sass', 'copy', 'js:build']);
+gulp.task('default', ['dev']);
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+var $ = require('jquery');
+var code = require('./inc/code');
+
+console.log(code(2332,112));
